58a2bd86ac72b9c4aadea20181c0b0bb3602d27e
Natan Maia <natanmaiaplays@outlook.com>
2024-09-15 10:34:47 -0300

    datagen step 2

--- src/main/java/com/nateplays/my_neoforge_mod/datagen/ModBlockStateProvider.java
+++ src/main/java/com/nateplays/my_neoforge_mod/datagen/ModBlockStateProvider.java
@@ -1,4 +1,28 @@
 package com.nateplays.my_neoforge_mod.datagen;
 
-public class ModBlockStateProvider {
+import com.nateplays.my_neoforge_mod.MyNeoForgeMod;
+import com.nateplays.my_neoforge_mod.block.ModBlocks;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.registries.DeferredBlock;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, MyNeoForgeMod.MODID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        blockWithItem(ModBlocks.EARTH_CRYSTAL_ORE);
+        blockWithItem(ModBlocks.MACHALITE_ORE);
+        blockWithItem(ModBlocks.MACHALITE_BLOCK);
+        blockWithItem(ModBlocks.MAGIC_BLOCK);
+//        blockWithItem(ModBlocks.NULBERRY_BUSH);
+
+    }
+
+    private void blockWithItem(DeferredBlock<?> deferredBlock) {
+        simpleBlockWithItem(deferredBlock.get(), cubeAll(deferredBlock.get()));
+    }
 }
--- src/main/java/com/nateplays/my_neoforge_mod/datagen/ModItemModelProvider.java
+++ src/main/java/com/nateplays/my_neoforge_mod/datagen/ModItemModelProvider.java
@@ -1,4 +1,22 @@
 package com.nateplays.my_neoforge_mod.datagen;
 
-public class ModItemModelProvider {
+import com.nateplays.my_neoforge_mod.MyNeoForgeMod;
+import com.nateplays.my_neoforge_mod.item.ModItems;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+
+public class ModItemModelProvider extends ItemModelProvider {
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, MyNeoForgeMod.MODID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        basicItem(ModItems.EARTH_CRYSTAL.get());
+        basicItem(ModItems.MALACHITE_CHUNK.get());
+        basicItem(ModItems.MALACHITE_INGOT.get());
+        basicItem(ModItems.NULBERRY.get());
+//        basicItem(ModItems.CHISEL.get()); //custom model in assets folder
+    }
 }
